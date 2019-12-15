9497feb90132cbee70bc49f667bac5ef0ca2cd0c
Eric <Aerelioyanadel@gmail.com>
2019-12-15 07:53:05 -0500

    get yoga server running

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,19 @@
-// let's go!
+require ('dotenv').config({path: 'variables.env' }) 
+const createServer = require ('./createServer') 
+const db = require('./db')
+
+const server = createServer()
+
+//todo use express middleware to handle cookies (JWT)
+//todo use express middleware to populate current users 
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
+    },
+  },
+  deets => {
+  console.log(`Server is now running on PORT http:/locahost:${deets.port}`)
+})
