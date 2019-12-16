f366f5f08556998fd7e791314e5157f41a755a53
Eric <Aerelioyanadel@gmail.com>
2019-12-15 21:59:13 -0500

    apollo creaton complete

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,17 +1,31 @@
 import App, { Container } from 'next/app'
 import Page from '../components/Page'
+import { ApolloProvider } from 'react-apollo'
+import withData from '../lib/withData'
+import { ApolloClient } from 'apollo-boost'
 
 class Myapp extends App {
+  static async getInitialProps({Component, ctx }){
+    let pageProps = {}
+    if(Component.getInitialProps){
+      pageProps = await Component.getInitialProps(ctx)
+    }
+    //this exposese the query to the user
+    pageProps.query = ctx.query
+    return { pageProps }
+  }
   render(){
-    const { Component } = this.props
+    const { Component, apollo, pageProps } = this.props
     
     return(
       <Container>
-        <Page>
-          <Component/>
-        </Page>
+        <ApolloProvider client={ApolloClient}>
+          <Page>
+            <Component {...pageProps} />
+          </Page>
+        </ApolloProvider>
       </Container>
-    )
+    ) 
   }
 } 
 
